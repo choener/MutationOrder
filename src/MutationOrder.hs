36b8bc2ca411dde293ed48dc9f7838551d59741e
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-03-01 13:31:32 +0100

    fixed up command line parser help

--- src/MutationOrder.hs
+++ src/MutationOrder.hs
@@ -48,7 +48,7 @@ oOptions = Options
   , cooptcount    = 100000
   , cooptprint    = 2
   , outprefix     = "tmp"
-  , scoretype     = Centroid &= help "choose 'mfe', 'centroid', or 'pairdist' for the evaluation of each mutational step"
+  , scoretype     = Centroid &= help "choose 'mfe', 'centroid', 'pairdistmfe', or 'pairdistcen' for the evaluation of each mutational step"
   , positivesquared = False &= help "square positive energies to penalize worse structures"
   , onlypositive  = False &= help "minimize only over penalties, not energy gains"
   , posscaled     = Nothing
